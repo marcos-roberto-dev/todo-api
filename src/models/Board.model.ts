0523ca7ef01c9839fd69d9924f361f4e9812c7bc
Marcos Roberto <marcosprojetosdev@gmail.com>
2023-10-27 08:28:17 -0300

    :recycle: refactor models added abstract ModelBase

--- src/models/Board.model.ts
+++ src/models/Board.model.ts
@@ -1,8 +1,12 @@
-export default class BoardModel {
+import ModelBase from "../types/ModelBase.type";
+
+export default class BoardModel extends ModelBase {
   constructor(
     public id: string,
     public name: string,
     public description: string,
     public columnsId: string[]
-  ) {}
+  ) {
+    super();
+  }
 }
---
+++ src/types/ModelBase.type.ts
@@ -0,0 +1,5 @@
+export default abstract class ModelBase {
+  abstract id: string;
+  abstract name: string;
+  abstract description: string;
+}
