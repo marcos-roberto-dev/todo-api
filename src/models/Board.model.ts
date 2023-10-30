b8b57d74afeb01083939fb83cb2901d6d5f28bfd
Marcos Roberto <marcosprojetosdev@gmail.com>
2023-10-30 07:22:31 -0300

    :recycle: add a new property in board _todosId

--- src/models/Board.model.ts
+++ src/models/Board.model.ts
@@ -2,22 +2,33 @@ import ModelBase from "../types/ModelBase.type";
 
 export default class Board extends ModelBase {
   protected _columnsId: string[];
+  protected _todosId: string[];
 
   constructor(
     id: string,
     name: string,
     description: string,
-    columnsId: string[]
+    columnsId: string[],
+    todosId: string[]
   ) {
     super(id, name, description);
     this._columnsId = columnsId;
+    this._todosId = todosId;
   }
 
   get columnsId(): string[] {
     return this._columnsId;
   }
 
+  get todosId(): string[] {
+    return this._todosId;
+  }
+
   setColumnsId(columnsId: string[]): void {
     this._columnsId = columnsId;
+  }
+
+  setTodosId(todosId: string[]): void {
+    this._todosId = todosId;
   }
 }
